deae81062c31c61369d8376696267fabd7f3a760
github username <github email address>
2019-02-18 12:12:56 -0800

    First commit.

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 class App < Sinatra::Base
 
-	get '/' do
+	get ('/') {
 		erb :index
-	end
+  }
+  
+  get ('/hello') {
+    erb :hello
+  }
 
+  get ('/goodbye') {
+    erb :goodbye
+  }
+
+  get ('/date') {
+    erb :date
+  }
 
 end
